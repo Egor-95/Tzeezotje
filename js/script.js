6ecfe5bef9f00a1f8e45c19a77f67d53e3b21d74
Egor Sporysh <egorsporush13791995@gmail.com>
2023-01-31 16:02:35 +0300

    tzeezotje

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// swiper
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+
+
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+
+
+});
+
+// Отправка заявки
+
